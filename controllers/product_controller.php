ba093f0ab84410f188fb3c3c97fe329eeede6ec7
Pedro <pedrosns13@gmail.com>
2020-09-08 15:55:23 +0200

    actualizacion product controller

--- controllers/product_controller.php
+++ controllers/product_controller.php
@@ -25,8 +25,8 @@ class ProductController extends BaseController {
 		$rules=[
 			'nombre'=> 'required',
 			'descripcion'=> 'required',
-			'costo'=> 'only_number',
-			'cantidad'=> 'only_number',
+			'costo'=> 'only_number|required',
+			'cantidad'=> 'only_number|required',
 		];
 		$errores = $this->validador->validate($data, $rules);		
 
